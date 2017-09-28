8de1c74fb91b3ff0aeda4372921e9a5012860adb
Volodymyr Kobeliatsky <volodymyrk@wix.com>
2017-09-28 18:27:46 +0300

    extended counter with increment decrement;

--- src/components/counter.tsx
+++ src/components/counter.tsx
@@ -2,6 +2,8 @@ import * as React from 'react';
 
 export interface Props {
     initial?: number;
+    increment?: number;
+    decrement?: number;
 }
 
 export interface State {
@@ -9,6 +11,12 @@ export interface State {
 }
 
 export default class Counter extends React.Component<Props, State> {
+
+    static defaultProps = {
+        increment: 1,
+        decrement: 1,
+    };
+
     constructor({initial = 0}: Props) {
         super();
         this.state = {
@@ -21,22 +29,28 @@ export default class Counter extends React.Component<Props, State> {
     }
     
     render() {
+        const {increment, decrement} = this.props;
         const {count} = this.state;
         return (
             <div>
                 <button
                     onClick={this.handleDecrement}
-                >-
+                    value={decrement}
+                >-{decrement}
                 </button>
                 count: {count}
                 <button
                     onClick={this.handleIncrement}
-                >+
+                    value={increment}
+                >+{increment}
                 </button>
             </div>
         );
     }
 
-    private handleDecrement = () => this.setState({count: this.state.count - 1});
-    private handleIncrement = () => this.setState({count: this.state.count + 1});
+    private handleDecrement: React.MouseEventHandler<HTMLButtonElement> = 
+        e => this.setState({count: this.state.count - Number(e.currentTarget.value)})
+
+    private handleIncrement: React.MouseEventHandler<HTMLButtonElement> = 
+        e => this.setState({count: this.state.count + Number(e.currentTarget.value)})
 }
