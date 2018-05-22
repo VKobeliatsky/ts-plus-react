0add01ce64f70cfcd055e255ac4363aeaa2e481e
Volodymyr Kobeliatsky <volodymyrk@wix.com>
2018-05-22 15:21:58 +0300

    added basics;

---
+++ src/basics.ts
@@ -0,0 +1,27 @@
+// tslint:disable
+
+interface Person {
+    firstName: string;
+    lastName: string;
+}
+
+function greeter(person : Person) {
+    return "Hello, " + person.firstName + " " + person.lastName;
+}
+
+class Student {
+    fullName: string;
+    protected socialSecurityNumber: number;
+    constructor(
+        public firstName: string,
+        public middleInitial: string,
+        public lastName: string
+    ) {
+        this.fullName = firstName + " " + middleInitial + " " + lastName;
+        this.socialSecurityNumber = Math.random();
+    }
+}
+
+let user = new Student("Jane", "M.", "User");
+
+document.body.innerHTML = greeter(user);
