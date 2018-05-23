9f338b0ac74d56f5e4785b8b73306cc964f37367
Volodymyr Kobeliatsky <volodymyrk@wix.com>
2018-05-23 15:09:34 +0300

    fixed actions reference in coiunter;

--- src/components/counter.tsx
+++ src/components/counter.tsx
@@ -1,6 +1,6 @@
 import * as React from 'react';
 import { connect } from 'react-redux';
-import { increment, decrement } from '../actions';
+import { increment, decrement } from '../store/actions';
 
 export interface Props {
     count: number;
