a9a92de41b7603148b5ca1acdf5d34203edb99ef
Volodymyr Kobeliatsky <volodymyrk@wix.com>
2018-05-23 14:46:31 +0300

    moved store files into separate folder;

--- src/index.tsx
+++ src/index.tsx
@@ -1,12 +1,10 @@
 import * as React from 'react';
 import * as ReactDOM from 'react-dom';
-import { createStore } from 'redux';
 import { Provider } from 'react-redux';
-import { counter } from './reducers';
+
+import store from './store';
 import App from './App';
 import './index.css';
-
-const store = createStore(counter);
 
 ReactDOM.render(
   <Provider store={store}>
